994feab5d3eabb141751ff6ab2c58e3384797729
fatma <fatmabmarzoug@gmail.com>
2024-09-04 12:42:29 +0200

    feat: add `todo` router in `router` folder

---
+++ src/router/todo/index.ts
@@ -0,0 +1,49 @@
+import express from "express";
+
+import { postTodo } from "../../controllers/todo";
+
+const router = express.Router();
+
+
+/**
+ * @swagger
+ * /todo:
+ *   post:
+ *     summary: return todo list
+ *     tags: [todos]
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               text:
+ *                 type: string
+ *               user:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: ok
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       404:
+ *         description: not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       500:
+ *         description: error
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *
+ */
+router.route("/todo").post(postTodo);
+
+
+
+export default router;
